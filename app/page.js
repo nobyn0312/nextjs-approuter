122af9beda6e364b6ec1a4f50bba5e0726e23d19
nobyn0312 <nobyn0312@gmail.com>
2024-07-19 18:40:49 +0900

    css

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import Image from "next/image"
 import Link from "next/link"
+import style from "./style.css"
 
 export default function Page(){
   return(
     <>
       <h1>page</h1>
+      <Image src="/next.svg" width={150} height={150}/>
       <p> <Link href="about">ポートフォリオ</Link></p>
     </>
   )
